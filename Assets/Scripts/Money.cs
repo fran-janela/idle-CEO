0e7234db3fde0f0df92d6d89f505a912490cff60
Lihsayuri <liviasm1@al.insper.edu.br>
2023-05-24 10:44:31 -0300

    Popup tá bonitinho!

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float money;
+    void Start()
+    {   
+        money = 1200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/buyUpgrade.cs
+++ Assets/Scripts/buyUpgrade.cs
@@ -2,28 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class buyUpgrade : MonoBehaviour
 {
-    public float money;
+    public TextMeshProUGUI moneyText;
+
+    public TextMeshProUGUI levelText;
 
     public Image BuyBar; 
-    public int multiplier;
-    // Start is called before the first frame update
+    public float multiplier;
+
+    public int level = 0;
+
+    public CanvasGroup canvasGroup;
+
+    public Button button;
+
+    public Money moneyScript;
+
+
     void Start()
     {
+        moneyScript = GameObject.FindObjectOfType<Money>();
+        canvasGroup.alpha = 0.2f;
         BuyBar.fillAmount = 0;
-        money = 1000;
-        multiplier = 1;
+        multiplier = 0f;
+        moneyText.text = moneyScript.money.ToString();
+        levelText.text = "0";
+
+        Image buttonImage = button.GetComponent<Image>();
+        Color buttonColor = buttonImage.color;
+        buttonColor.a = 1.0f;
+        buttonImage.color = buttonColor;
         
     }
 
     public void buy()
     {
-        if (money >= 200){
-            money -= 200;
-            multiplier += 1;
-            BuyBar.fillAmount += 0.1f;
+        if (BuyBar.fillAmount == 1f){
+            level += 1;
+            BuyBar.fillAmount = 0;
+            
+            moneyScript.money += 100 * multiplier;
+            moneyText.text = moneyScript.money.ToString();
+            levelText.text = level.ToString();
+        } 
+        if (moneyScript.money >= 200){
+            canvasGroup.alpha = 1f;
+            moneyScript.money -= 200;
+            multiplier += 0.25f;
+            BuyBar.fillAmount += 0.25f;
+            moneyText.text = moneyScript.money.ToString();
+            // hasItem = true;
+            // mudarTransparencia(1f);
         } else {
             Debug.Log("Not enough money");
         }
