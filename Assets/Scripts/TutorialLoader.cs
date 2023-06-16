91ba773af4ce8ed2262668f7c2a097924b64d079
Lihsayuri <liviasm1@al.insper.edu.br>
2023-06-16 10:25:06 -0300

    Adicionando as cenas de tutorial

---
+++ Assets/Scripts/TutorialLoader.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public TextMeshProUGUI progrssText;
+
+    public void SceneLoader()
+    {
+        if (PlayerPrefs.GetInt("Tutorial") == 0)
+        {
+            PlayerPrefs.SetInt("Tutorial", 1);
+            StartCoroutine(LoadAsynchronously(1));
+            // UnityEngine.SceneManagement.SceneManager.LoadScene("Tutorial");
+        }
+        else
+        {
+            StartCoroutine(LoadAsynchronously(2));
+            // UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+
+    IEnumerator LoadAsynchronously(int index)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progrssText.text = progress * 100f + "%";
+            Debug.Log(progress);
+            yield return null;
+        }
+    }
+
+
+}
